90bb83e2356b7296ff05cb1a9ae1b8f5ecb7f041
dwillcox <dwillcox@electra.iacs.stonybrook.edu>
2016-09-22 07:27:15 -0400

    Make idfile a required argument instead of an option.

--- artoo_driver.py
+++ artoo_driver.py
@@ -25,9 +25,9 @@ import argparse
 from Artoo import Artoo
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-idfile', '--identityfile', type=str, help='Bot identity file from which to read the Bot ID and Token.')
+parser.add_argument('idfile', type=str, help='Bot identity file from which to read the Bot ID and Token.')
 parser.add_argument('-watch', '--watch', action='store_true', help='Watch all slack messages and print them to the console.')
 args = parser.parse_args()
 
-artoo = Artoo(args.identityfile, args.watch)
+artoo = Artoo(args.idfile, args.watch)
 artoo.poll_slack()
